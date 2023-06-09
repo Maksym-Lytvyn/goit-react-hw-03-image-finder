eda0bf44cef19d34d1a008064c6a9943b129a7b9
Максим Литвин / Maksym Lytvyn <104524765+Maksym-Lytvyn@users.noreply.github.com>
2023-06-09 21:37:33 +0300

    up

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,2 +1,4 @@
-const Error = ({ error }) => <div>{error}</div>;
+import ErrorCSS from './Error.module.css'
+
+const Error = ({ error }) => <div className={ErrorCSS.error_styled}>{error}</div>;
 export default Error;
